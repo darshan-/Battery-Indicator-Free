e8a15ce46d7975abc20eac7f2276db08c6a7eabd
Darshan-Josiah Barber <darshan@darshancomputing.com>
2011-07-04 22:06:19 +0000

    [Free]: Finished purge_unused_strings.rb; made Free APK less than 2/3 previous size.
    git-svn-id: file:///home/darshan/svn-repo-clone-of-bi-on-google-code/branches/free@422 fb11ccbe-488a-4026-b0b0-a5194d29edc0

--- purge_unused_strings.rb
+++ purge_unused_strings.rb
@@ -5,12 +5,23 @@ require 'nokogiri'
 string_names = {}
 string_files = []
 
-`grep R.string src/com/darshancomputing/BatteryIndicator/*.java`.each_line do |s|
-  string_names[s.match(/R.string.([a-z_]*)/)[1]] = true
-end
+data = [{:matches => `grep R.string src/com/darshancomputing/BatteryIndicator/*.java`,
+          :regex => /R.string.([a-z_]*)/},
+        {:matches => `grep -R --exclude-dir=.svn @string res AndroidManifest.xml`,
+          :regex => /@string\/([a-z_]*)/}]
 
-`grep -R --exclude-dir=.svn @string res AndroidManifest.xml`.each_line do |s|
-  string_names[s.match(/@string\/([a-z_]*)/)[1]] = true
+data.each do |set|
+  set[:matches].each_line do |s|
+    r = set[:regex]
+
+    while(true)
+      m = s.match(r)
+      break if not m
+
+      string_names[m[1]] = true
+      s = m.post_match
+    end
+  end
 end
 
 Dir.glob('res/values*').each do |d|
@@ -20,8 +31,22 @@ end
 string_files.sort!
 
 string_files.each do |file|
+  puts "Editing " << file
+
   strings_xml = ""
   File.open(file) {|f| strings_xml = f.read()}
 
   doc = Nokogiri::XML(strings_xml)
+
+  doc.xpath('//string').each do |str_el|
+    if not string_names[str_el.attr('name')] then
+      puts " - Deleting " << str_el.attr('name')
+      str_el.remove()
+    end
+  end
+
+  s = doc.to_xml(:encoding => 'utf-8')
+  s = s.gsub(/(\n\s+\n)+/, "\n")
+
+  File.open(file, 'w') {|f| f.write(s)}
 end
