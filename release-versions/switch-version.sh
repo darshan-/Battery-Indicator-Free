c7a0de617c1c4a7211402ec1b65f7d26427883e9
Darshan-Josiah Barber <darshan@darshancomputing.com>
2012-04-20 16:15:42 -0700

    Continue working on version switcher. Test out just including the icons outright under drawable-v11, which actually seems like the best option.  Needs further testing on more devices.

--- release-versions/switch-version.sh
+++ release-versions/switch-version.sh
@@ -1,20 +1,44 @@
 #!/bin/bash
 
-cd ../default/res
+# cd current
+# for each file in each dir in current, remove it from actual res/
+# go back up
+# swap current
+# cd current
+# for each file in each dir in current, copy to actual res/
 
-for d in values*
-do
-    source=$d/strings.xml
-    dest_dir=../../free/res/$d
-
-    if [ -e $source ]
+function swap_current {
+    if [ -e `realpath` ]
     then
-        if [ ! -d $dest_dir ]
-        then
-            echo "Creating res/$d"
-            mkdir $dest_dir
-        fi
-
-        cp $source $dest_dir
     fi
+}
+
+cd current
+
+for f in */*
+do
+    echo rm ../../res/$f
 done
+
+cd ..
+
+
+
+# cd ../default/res
+
+# for d in values*
+# do
+#     source=$d/strings.xml
+#     dest_dir=../../free/res/$d
+
+#     if [ -e $source ]
+#     then
+#         if [ ! -d $dest_dir ]
+#         then
+#             echo "Creating res/$d"
+#             mkdir $dest_dir
+#         fi
+
+#         cp $source $dest_dir
+#     fi
+# done
