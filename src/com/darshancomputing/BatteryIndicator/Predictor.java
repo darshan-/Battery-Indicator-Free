271019e0ab89769c09127de13fdb4df17518953a
Darshan-Josiah Barber <darshan@darshancomputing.com>
2013-04-27 17:05:01 -0700

    Pulled in updated Predictor code from Pro, updated Dutch and Italian translations, released Free-8.0.6 (Beta).

--- src/com/darshancomputing/BatteryIndicator/Predictor.java
+++ src/com/darshancomputing/BatteryIndicator/Predictor.java
@@ -16,8 +16,7 @@ package com.darshancomputing.BatteryIndicator;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-
-import java.util.LinkedList;
+import android.os.SystemClock;
 
 public class Predictor {
     private static final int DISCHARGE    = 0;
@@ -25,33 +24,66 @@ public class Predictor {
     private static final int RECHARGE_WL  = 2;
     private static final int RECHARGE_USB = 3;
 
-    private double[] average = new double[4];
+    public static final int ONE_MINUTE      = 60 * 1000;
+    public static final int FIVE_MINUTES    = ONE_MINUTE *  5;
+    public static final int TEN_MINUTES     = ONE_MINUTE * 10;
+    public static final int FIFTEEN_MINUTES = ONE_MINUTE * 15;
+    public static final int THIRTY_MINUTES  = ONE_MINUTE * 30;
+    public static final int ONE_HOUR        = ONE_MINUTE * 60;
+    public static final int TWO_HOURS       = ONE_HOUR *  2;
+    public static final int THREE_HOURS     = ONE_HOUR *  3;
+    public static final int FOUR_HOURS      = ONE_HOUR *  4;
+    public static final int SIX_HOURS       = ONE_HOUR *  6;
+    public static final int EIGHT_HOURS     = ONE_HOUR *  8;
+    public static final int TWELVE_HOURS    = ONE_HOUR * 12;
+    public static final int ONE_DAY         = ONE_HOUR * 24;
+    public static final int SINCE_STATUS_CHANGE = -1;
+
+    public static final int TYPE_FIVE_MINUTES    =  1;
+    public static final int TYPE_TEN_MINUTES     =  2;
+    public static final int TYPE_FIFTEEN_MINUTES =  3;
+    public static final int TYPE_THIRTY_MINUTES  =  4;
+    public static final int TYPE_ONE_HOUR        =  5;
+    public static final int TYPE_TWO_HOURS       =  6;
+    public static final int TYPE_THREE_HOURS     =  7;
+    public static final int TYPE_FOUR_HOURS      =  8;
+    public static final int TYPE_SIX_HOURS       =  9;
+    public static final int TYPE_EIGHT_HOURS     = 10;
+    public static final int TYPE_TWELVE_HOURS    = 11;
+    public static final int TYPE_ONE_DAY         = 12;
+    public static final int TYPE_STATUS_CHANGE   = 13;
+    public static final int TYPE_LONG_TERM       = 14;
+
+    private static final double WEIGHT_OLD_AVERAGE = 0.998;
+    private static final double WEIGHT_NEW_DATA =  1 - WEIGHT_OLD_AVERAGE;
 
     private static final int[] DEFAULT = { 24 * 60 * 60 * 1000 / 100,
-                                           3 * 60 * 60 * 1000 / 100,
-                                           4 * 60 * 60 * 1000 / 100,
-                                           6 * 60 * 60 * 1000 / 100 };
+                                            3 * 60 * 60 * 1000 / 100,
+                                            4 * 60 * 60 * 1000 / 100,
+                                            6 * 60 * 60 * 1000 / 100 };
 
     private static final String[] KEY_AVERAGE = { "key_ave_discharge",
                                                   "key_ave_recharge_ac",
                                                   "key_ave_recharge_wl",
                                                   "key_ave_recharge_usb" };
 
-    private static final int RECENT_DURATION = 5 * 60 * 1000;
+    private int recent_duration = FIVE_MINUTES;
+    private int prediction_type = TYPE_FIVE_MINUTES;
 
-    private static final double WEIGHT_OLD_AVERAGE = 0.998;
-    private static final double WEIGHT_NEW_DATA =  1 - WEIGHT_OLD_AVERAGE;
+    private long[] timestamps = new long[101];
+    private int ts_head;
 
-    private LinkedList<Double> recents;
+    private double[] average = new double[4];
 
-    private long last_ms;
-    private int last_level;
-    private int last_status;
-    private int last_plugged;
-    private int last_index;
     private double recent_average;
+
+    private int last_level;
+    private int last_status = -1; // Impossible value, so first update knows it's the first update
+    private int last_plugged;
+    private int last_charging_status;
+    private int dir_inc; // -1 if charging; 1 if discharging; unspecified otherwise. For iterating over timestamps.
+    private long now;
     private boolean partial;
-    private boolean initial;
 
     private SharedPreferences sp_store;
     private SharedPreferences.Editor editor;
@@ -64,68 +96,66 @@ public class Predictor {
         average[RECHARGE_AC]  = sp_store.getFloat(KEY_AVERAGE[RECHARGE_AC],  DEFAULT[RECHARGE_AC]);
         average[RECHARGE_WL]  = sp_store.getFloat(KEY_AVERAGE[RECHARGE_WL],  DEFAULT[RECHARGE_WL]);
         average[RECHARGE_USB] = sp_store.getFloat(KEY_AVERAGE[RECHARGE_USB], DEFAULT[RECHARGE_USB]);
-
-        recents = new LinkedList<Double>();
     }
 
     public void update(BatteryInfo info) {
-        if (info.status != last_status || info.plugged != last_plugged || last_ms == 0 || info.status == BatteryInfo.STATUS_FULLY_CHARGED) {
-            recents.clear();
+        now = SystemClock.elapsedRealtime();
+
+        if (info.status != last_status ||
+            info.plugged != last_plugged ||
+            info.status == BatteryInfo.STATUS_FULLY_CHARGED ||
+            (info.status == BatteryInfo.STATUS_CHARGING && info.percent < ts_head) ||
+            (info.status == BatteryInfo.STATUS_DISCHARGING && info.percent > ts_head))
+        {
+            ts_head = info.percent;
+            dir_inc = info.status == BatteryInfo.STATUS_CHARGING ? -1 : 1;
             partial = false;
-            initial = true;
+
+            timestamps[info.percent] = now;
+
             setLasts(info);
             updateInfoPrediction(info);
             return;
         }
 
         if ((info.status == BatteryInfo.STATUS_CHARGING && info.percent < last_level) ||
-            (info.status != BatteryInfo.STATUS_CHARGING && info.percent > last_level))
+            (info.status == BatteryInfo.STATUS_DISCHARGING && info.percent > last_level))
         {
-            // There may be better ways to account for this backslide, but the simplest solution is to just ignore it
+            partial = false;
+            setLasts(info);
+            updateInfoPrediction(info);
             return;
         }
 
-        int status = indexFor(info.status, info.plugged);
-        int level_diff = java.lang.Math.abs(last_level - info.percent);
-        double ms_diff = (double) (System.currentTimeMillis() - last_ms);
+        int level_diff = Math.abs(last_level - info.percent);
+        double ms_diff = (double) (now - timestamps[last_level]);
 
         if (level_diff == 0) {
+            partial = true;
             if (ms_diff <= recent_average)
                 return;
-
-            if (partial) {
-                recents.set(0, ms_diff);
-            } else {
-                recents.addFirst(ms_diff);
-                partial = true;
-            }
         } else {
-            if (partial) {
-                recents.remove(0);
-                partial = false;
-            }
+            partial = false;
+            double ms_per_point = ms_diff / level_diff;
+            int charging_status = chargingStatusFor(info.status, info.plugged);
 
-            ms_diff /= level_diff;
+            for (int i = 0; i < level_diff; i += 1)
+                timestamps[info.percent + (i * dir_inc)] = now - (long) (i * ms_per_point);
 
-            if (initial && ms_diff < average[status]) { // Non-useful initial level change after status change
-                initial = false;
+            // Initial level change may happen promptly and should not shorten prediction
+            if (Math.abs(ts_head - info.percent) <= 1 && ms_per_point < recent_average) {
                 setLasts(info);
                 return;
             }
 
-            initial = false;
-
             for (int i = 0; i < level_diff; i++) {
-                recents.addFirst(ms_diff);
-
-                average[status] = average[status] * WEIGHT_OLD_AVERAGE + ms_diff * WEIGHT_NEW_DATA;
-                editor.putFloat(KEY_AVERAGE[status], (float) average[status]);
+                average[charging_status] = average[charging_status] * WEIGHT_OLD_AVERAGE + ms_per_point * WEIGHT_NEW_DATA;
+                editor.putFloat(KEY_AVERAGE[charging_status], (float) average[charging_status]);
             }
-
-            setLasts(info);
             editor.commit();
         }
 
+        setLasts(info);
         updateInfoPrediction(info);
     }
 
@@ -147,7 +177,9 @@ public class Predictor {
 
         double predicted = recentAverage();
 
-        return (int) (predicted * last_level / 1000);
+        int level = last_level;
+        if (partial) level -= dir_inc;
+        return (int) (predicted * level / 1000);
     }
 
     public int secondsUntilCharged() {
@@ -159,35 +191,38 @@ public class Predictor {
             return -1;
         }
 
-        return (int) ((100 - last_level) * recentAverage() / 1000);
+        int level = last_level;
+        if (partial) level -= dir_inc;
+        return (int) ((100 - level) * recentAverage() / 1000);
     }
 
     private void setLasts(BatteryInfo info) {
-        last_level = info.percent;
+        last_level = info.percent; // TODO: Resolve level/percent discrepancy?
         last_status = info.status;
         last_plugged = info.plugged;
-        last_index = indexFor(last_status, last_plugged);
-        last_ms = System.currentTimeMillis();
+        last_charging_status = chargingStatusFor(last_status, last_plugged);
     }
 
     private double recentAverage() {
         double total_points = 0d;
         double total_ms = 0d;
-        double needed_ms = RECENT_DURATION;
+        double needed_ms = recent_duration;
 
-        int i;
-        for (i = 0; i < recents.size(); i++) {
-            double t = recents.get(i);
+        int start = last_level;
+        if (partial) start -= dir_inc;
+
+        for (int i = start; i != ts_head; i += dir_inc) {
+            double t;
+
+            if (i == start && partial)
+                t = now - timestamps[last_level];
+            else
+                t = timestamps[i] - timestamps[i + dir_inc];
 
             if (t > needed_ms) {
                 total_points += needed_ms / t;
                 total_ms += needed_ms;
                 needed_ms = 0;
-
-                i++;
-                while (recents.size() > i) // This is a convenient place to trim recents
-                    recents.remove(i);
-
                 break;
             }
 
@@ -197,13 +232,13 @@ public class Predictor {
         }
 
         if (needed_ms > 0)
-            total_points += needed_ms / average[last_index];
+            total_points += needed_ms / average[last_charging_status];
 
-        recent_average = RECENT_DURATION / total_points;
+        recent_average = recent_duration / total_points;
         return recent_average;
     }
 
-    private int indexFor(int status, int plugged) {
+    private int chargingStatusFor(int status, int plugged) {
         if (status == BatteryInfo.STATUS_CHARGING) {
             if (plugged == BatteryInfo.PLUGGED_USB)
                 return RECHARGE_USB;
